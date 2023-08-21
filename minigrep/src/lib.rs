38d9581c09d7baf0d4a6db4d5c3bafef00268ddb
tranphuocanh2007 <onlyway102@gmail.com>
2023-08-21 17:56:17 +0700

    Lesson 15: Iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,14 +25,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str)-> Vec<&'a str> {
@@ -48,16 +44,21 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str)-> Vec<&'a str
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3{
-            return Err("not enough arguments");
-        }
-        
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string",)
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
+            
         Ok(Config {query, file_path , ignore_case})
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,8 @@ use std::process;
 use minigrep::{Config, run};
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem with arguments: {}", err);
         process::exit(1);
     });
