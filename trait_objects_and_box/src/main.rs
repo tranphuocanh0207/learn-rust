65786c10ca5ae4b3663697cacbc481e028ae7355
tranphuocanh2007 <onlyway102@gmail.com>
2023-08-29 10:30:54 +0700

    Lesson 19: Trait Objects and Box

---
+++ trait_objects_and_box/src/main.rs
@@ -0,0 +1,31 @@
+trait Clicky {
+    fn click(&self) -> String;
+}
+
+struct Keyboard;
+impl Clicky for Keyboard {
+    fn click(&self) -> String {
+        "Click keyboard".to_owned()
+    }
+}
+
+struct Mouse;
+impl Clicky for Mouse {
+    fn click(&self) -> String {
+        "Click mouse".to_owned()
+    }
+}
+
+fn main() {
+    let a :Box<dyn Clicky>= Box::new(Keyboard);
+    println!("{}", a.click());
+
+    let b: Box<dyn Clicky> = Box::new(Mouse);
+    println!("{}", b.click());
+
+    let vec = vec![a,b];
+
+    for i in vec.iter() { 
+        println!("{}", i.click());
+    }
+}
